f0a1961c9ebc143f908e3009ba44259353b50c55
WangQitai <wangqitai@wangqitaidemacbook-pro.local>
2016-06-08 11:15:26 +0800

    Initial Commit

---
+++ MyStr/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MyStr
+//
+//  Created by WangQitai on 16/6/8.
+//  Copyright © 2016年 WangQitai. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
